0babd7b50eb1c93a077dd0e85a78c8f996119ea5
Harrison <103507184+hao441@users.noreply.github.com>
2023-10-30 13:26:53 +0900

    prod v4

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Approved Mobile Welding',
-  description: 'Part of the Approved Industries Company.',
+  title: 'Sydney Water Bike Rentals',
+  description: 'Water Bike Rentals',
 }
 
 export default function RootLayout({
